541dc87d3c0d546e42d66833e6f8a4fd3b38b750
Christie Yoon <cleex1999@gmail.com>
2020-08-20 01:44:58 +0000

    created API class

--- calories/lib/api.rb
+++ calories/lib/api.rb
@@ -1,26 +1,14 @@
-
+require 'uri'
 require 'net/http'
-require 'open-uri'
-require 'json'
-require 'restclient'
+require 'openssl'
 
-class API 
-   
-   URL = ".json"
-   
-   def self.get_recipes(calories)
-    response = RestClient::Request.execute(
-   :method => :get,
-   :url => "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/%7Bid%7D/nutritionWidget",
-   :headers => {
-	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-	"x-rapidapi-key": "24ffe50364msh82b342b5ced1400p13091ajsn8ec97e5992cc",
-	"accept": "text/html",
-	"useQueryString": true
-}
-)
-binding.pry
-   end
-  
-end
-  API.get_recipes(2)
+url = URI("https://api.spoonacular.com/recipes/complexSearch?apiKey=284f148e79944cf4b76961fd5d79ceec&minCalories=300&maxCalories=350")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+
+response = http.request(request)
+puts response.read_body
--- looping.rb
+++
@@ -1,3 +0,0 @@
-loop do
-   puts "Never pass a school bus if it has flashing red lights"
-end
