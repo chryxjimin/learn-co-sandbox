cba04f6e6209e801af766a787c2a7842a7aab413
Christie Yoon <cleex1999@gmail.com>
2020-08-22 21:58:46 +0000

    recommiting just in case changes didn't save

--- calories/lib/api.rb
+++ calories/lib/api.rb
@@ -7,8 +7,8 @@ require "json"
 class API
 
    def self.get_recipes(maxCalories)
-      url = URI("https://api.spoonacular.com/recipes/complexSearch?apiKey=284f148e79944cf4b76961fd5d79ceec&maxCalories=#{maxCalories}")
-
+     url = URI("https://api.spoonacular.com/recipes/complexSearch?apiKey=284f148e79944cf4b76961fd5d79ceec&maxCalories=#{maxCalories}")
+    
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -24,19 +24,17 @@ class API
       
       url = URI("https://api.spoonacular.com/recipes/#{id}/information?includeNutrition=false&apiKey=284f148e79944cf4b76961fd5d79ceec")
     
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
+      request = Net::HTTP::Get.new(url)
 
-    response = http.request(request)
-    recipe_information = JSON.parse(response.body)
+      response = http.request(request)
+      recipe_information = JSON.parse(response.body)
 
       puts "#{recipe_information["title"]}"
       puts "#{recipe_information["sourceUrl"]}"
-      
-      #binding.pry
     end
    end
  end
