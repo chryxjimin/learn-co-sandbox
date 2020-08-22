ca9a9ee3b0fb8f833745a7e26a6ac62704ad5cad
Christie Yoon <cleex1999@gmail.com>
2020-08-22 22:48:41 +0000

    created a new class Link

--- calories/lib/api.rb
+++ calories/lib/api.rb
@@ -18,24 +18,7 @@ class API
     response = http.request(request)
     response = JSON.parse(response.body)
   
-      # binding.pry
-      
-    response["results"].each.with_index(1) do |recipe, id|
-      
-      url = URI("https://api.spoonacular.com/recipes/#{id}/information?includeNutrition=false&apiKey=284f148e79944cf4b76961fd5d79ceec")
-    
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Get.new(url)
-
-      response = http.request(request)
-      recipe_information = JSON.parse(response.body)
-
-      puts "#{recipe_information["title"]}"
-      puts "#{recipe_information["sourceUrl"]}"
-    end
+      #binding.pry
    end
  end
 
