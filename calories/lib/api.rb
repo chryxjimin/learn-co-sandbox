ca4a1d23b55e0912a93473ecf95f0b26bd4d730e
Christie Yoon <cleex1999@gmail.com>
2020-08-22 04:12:40 +0000

    edited instance method

--- calories/lib/api.rb
+++ calories/lib/api.rb
@@ -20,8 +20,19 @@ class API
      
       # binding.pry
       
-    response["results"].each.with_index(1) do |r, index|
-      puts "#{index} - #{r["title"]}"
+    response["results"].each.with_index(1) do |recipe|
+      url = URI("https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=284f148e79944cf4b76961fd5d79ceec")
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+     request = Net::HTTP::Get.new(url)
+
+     response = http.request(request)
+     recipe_information = JSON.parse(response.body)
+      puts "#{recipe["title"]}"
+      puts "#{recipe["sourceUrl"]}"
+      binding.pry
     end
    end
 end
